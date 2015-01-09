0360a15af8c985b1f95a41c9e654b1235afce22f
Hesheng Zhou <hzhou@ilegendsoft.com>
2015-01-09 16:48:34 +0800

    update last framework
    update last framework

--- Chapter05_PrintingPostcard/Chapter05_PrintingPostcard/ViewController.m
+++ Chapter05_PrintingPostcard/Chapter05_PrintingPostcard/ViewController.m
@@ -24,6 +24,155 @@
 
 @synthesize imageView;
 
+// 多通道彩色图片的直方图比对
+-(double)CompareHist:(IplImage*)image1 withParam2:(IplImage*)image2
+{
+    int hist_size = 256;
+    float range[] = {0,255};
+    
+    IplImage *gray_plane = cvCreateImage(cvGetSize(image1), 8, 1);
+    cvCvtColor(image1, gray_plane, CV_BGR2GRAY);
+    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&gray_plane, gray_hist);
+    
+    IplImage *gray_plane2 = cvCreateImage(cvGetSize(image2), 8, 1);
+    cvCvtColor(image2, gray_plane2, CV_BGR2GRAY);
+    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&gray_plane2, gray_hist2);
+    
+    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
+}
+
+
+// 单通道彩色图片的直方图
+-(double)CompareHistSignle:(IplImage*)image1 withParam2:(IplImage*)image2
+{
+    int hist_size = 256;
+    float range[] = {0,255};
+    
+    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&image1, gray_hist);
+    
+    
+    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&image2, gray_hist2);
+    
+    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
+}
+
+
+
+// 进行肤色检测
+-(void)SkinDetect:(IplImage*)src withParam:(IplImage*)dst
+{
+    // 创建图像头
+    IplImage* hsv = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);//用于存图像的一个中间变量，是用来分通道用的，分成hsv通道
+    IplImage* tmpH1 = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);//通道的中间变量，用于肤色检测的中间变量
+    IplImage* tmpS1 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpH2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpS2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpH3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpS3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* H = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* S = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* V = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* src_tmp1=cvCreateImage(cvGetSize(src),8,3);
+    
+    // 高斯模糊
+    cvSmooth(src,src_tmp1,CV_GAUSSIAN,3,3); //高斯模糊
+    
+    // hue色度，saturation饱和度，value纯度
+    cvCvtColor(src_tmp1, hsv, CV_BGR2HSV );//颜色转换
+    cvSplit(hsv,H,S,V,0);//分为3个通道
+    /*********************肤色检测部分**************/
+    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(20.0,0.0,0,0),tmpH1);
+    cvInRangeS(S,cvScalar(75.0,0.0,0,0),cvScalar(200.0,0.0,0,0),tmpS1);
+    cvAnd(tmpH1,tmpS1,tmpH1,0);
+    
+    // Red Hue with Low Saturation
+    // Hue 0 to 26 degree and Sat 20 to 90
+    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(13.0,0.0,0,0),tmpH2);
+    cvInRangeS(S,cvScalar(20.0,0.0,0,0),cvScalar(90.0,0.0,0,0),tmpS2);
+    cvAnd(tmpH2,tmpS2,tmpH2,0);
+    
+    // Red Hue to Pink with Low Saturation
+    // Hue 340 to 360 degree and Sat 15 to 90
+    cvInRangeS(H,cvScalar(170.0,0.0,0,0),cvScalar(180.0,0.0,0,0),tmpH3);
+    cvInRangeS(S,cvScalar(15.0,0.0,0,0),cvScalar(90.,0.0,0,0),tmpS3);
+    cvAnd(tmpH3,tmpS3,tmpH3,0);
+    
+    // Combine the Hue and Sat detections
+    cvOr(tmpH3,tmpH2,tmpH2,0);
+    cvOr(tmpH1,tmpH2,tmpH1,0);
+    
+    cvCopy(tmpH1,dst);
+    
+    cvReleaseImage(&hsv);
+    cvReleaseImage(&tmpH1);
+    cvReleaseImage(&tmpS1);
+    cvReleaseImage(&tmpH2);
+    cvReleaseImage(&tmpS2);
+    cvReleaseImage(&tmpH3);
+    cvReleaseImage(&tmpS3);
+    cvReleaseImage(&H);
+    cvReleaseImage(&S);
+    cvReleaseImage(&V);
+    cvReleaseImage(&src_tmp1);
+}
+
+
+
+/// UIImage类型转换为IPlImage类型
+-(IplImage*)convertToIplImage:(UIImage*)image
+{
+    CGImageRef imageRef = image.CGImage;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    IplImage *iplImage = cvCreateImage(cvSize(image.size.width, image.size.height), IPL_DEPTH_8U, 4);
+    CGContextRef contextRef = CGBitmapContextCreate(iplImage->imageData, iplImage->width, iplImage->height, iplImage->depth, iplImage->widthStep, colorSpace, kCGImageAlphaPremultipliedLast|kCGBitmapByteOrderDefault);
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, image.size.width, image.size.height), imageRef);
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    IplImage *ret = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplImage, ret, CV_RGB2BGR);
+    cvReleaseImage(&iplImage);
+    return ret;
+}
+
+/// IplImage类型转换为UIImage类型
+-(UIImage*)convertToUIImage:(IplImage*)image
+{
+    cvCvtColor(image, image, CV_BGR2RGB);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    NSData *data = [NSData dataWithBytes:image->imageData length:image->imageSize];
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
+    CGImageRef imageRef = CGImageCreate(image->width, image->height, image->depth, image->depth * image->nChannels, image->widthStep, colorSpace, kCGImageAlphaNone | kCGBitmapByteOrderDefault, provider, NULL, false, kCGRenderingIntentDefault);
+    UIImage *ret = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    return ret;
+}
+
+
+- (void)checkSimilarity {
+    IplImage *i1, *i2;
+    
+    UIImage* image1 = [UIImage imageNamed:@"IMG_0150.jpg"];
+    i1 = [self convertToIplImage:image1];
+    UIImage* image2 = [UIImage imageNamed:@"IMG_0151.jpg"];
+    i2 = [self convertToIplImage:image2];
+
+    double res = [self CompareHist:i1 withParam2:i2];
+    NSLog(@"res is %f", res);
+}
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self checkSimilarity];
+}
+
 - (NSInteger)supportedInterfaceOrientations
 {
     // Only portrait orientation
@@ -44,7 +193,7 @@
     image = [UIImage imageNamed:@"texture.jpg"];
     UIImageToMat(image, params.texture);
     cvtColor(params.texture, params.texture, CV_RGBA2RGB);
-    
+
     // Load image with text
     image = [UIImage imageNamed:@"text.png"];
     UIImageToMat(image, params.text, true);
